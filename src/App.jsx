833040d14ddd0b7047e50d0f4335212b444b96b3
Jean <jeandehorta@outlook.com>
2023-08-07 19:30:57 -0500

    Colecciones React

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 
 import './App.css'
 import Card from './componentes/Card'
+import vehicles from './data/vehicles';
+
 
 function App() {
+  const vehiclesList = vehicles.map(v=> {
+    return <Card title={v.name} description={v.description}/>
+  })
   return <div className='App'>
     <h1>Hola Mundo</h1>
     <div className="container">
-    <Card title="Aqui Titulo" 
-    description="la descripcion es aqui"/>
-    <Card title="Aqui Titulo2" 
-    description="la descripcion es aqui2"/>
-    <Card title="Aqui Titulo3" 
-    description="la descripcion es aqui3"/>
-    <Card />
+    {vehiclesList}
     </div>
   </div>
    
